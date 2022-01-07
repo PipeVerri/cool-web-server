007a0311cfdbf086f4eb2ae03b0563e2c327bc92
PipeVerri <pipe.verri@gmail.com>
2022-01-07 13:14:29 -0300

    Get the content for the request

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import re  # for parsing strings
 #           Configuration           #
 #####################################
 
-websiteDirectory = "./run_directory/"  # where will the website run
+websiteDirectory = "./run_directory"  # where will the website run
 defaultFile = "index.html"  # what file send by default to the site
 
 #####################################
@@ -17,8 +17,19 @@ defaultFile = "index.html"  # what file send by default to the site
 #####################################
 
 
-def get_request_information(data):
-    print(data)
+# we will use this function in the future for rendering other formats, like php
+def read_document(path):
+    data = open(path, "r").read()
+    return data
+
+
+# we will use this function for getting the content for the client
+def get_file_data(data):
+    file_path = data.split("\n").split(" ")[1]  # we get the first line, and then we get the second element
+    if file_path == "":  # if the file is /, the client is asking for the default document
+        file_path = defaultFile
+    file_data = read_document(websiteDirectory + file_path)
+    return file_data
 
 
 ######################################
@@ -26,10 +37,10 @@ def get_request_information(data):
 ######################################
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create a normal tcp socket
-serverSocket.bind((socket.gethostname(), 8080))  # we bind it to our real ip to make it visible to the real world
+serverSocket.bind(("0.0.0.0", 8080))  # we bind it to our real ip to make it visible to the real world
 serverSocket.listen(10)  # we start listening and accept 10 concurrent connections
 
 while True:
     clientSocket, clientAddress = serverSocket.accept()
     request = clientSocket.recv(1024)
-    get_request_information(request.decode())
+    content = get_file_data(request.decode())
