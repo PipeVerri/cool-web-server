172cfb2b88517f3eadbb7a09101ef6ac2ebccea8
PipeVerri <pipe.verri@gmail.com>
2022-01-07 12:20:02 -0300

    Created the structure for the server
    I hope in the future the structure will work for all the features

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
-import socket
-import os
+#####################################
+#              Imports              #
+#####################################
 
-path = "./run_directory"
+import socket  # for sending and receiving data
+import re  # for parsing strings
 
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serverSocket.bind(("0.0.0.0", 8080))
-serverSocket.listen(10)
+#####################################
+#           Configuration           #
+#####################################
+
+websiteDirectory = "./run_directory/"  # where will the website run
+defaultFile = "index.html"  # what file send by default to the site
+
+#####################################
+#            Functions              #
+#####################################
+
+
+def get_request_information(data):
+    print(data)
+
+
+######################################
+#           Server Socket            #
+######################################
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # create a normal tcp socket
+serverSocket.bind((socket.gethostname(), 8080))  # we bind it to our real ip to make it visible to the real world
+serverSocket.listen(10)  # we start listening and accept 10 concurrent connections
 
 while True:
     clientSocket, clientAddress = serverSocket.accept()
-    data = clientSocket.recv(1024)
-    print(data)
+    request = clientSocket.recv(1024)
+    get_request_information(request.decode())
