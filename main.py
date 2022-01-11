1e8b89c06fa9e748aea1d24a9940c426eb0ff8e0
PipeVerri <pipe.verri@gmail.com>
2022-01-11 13:53:37 -0300

    Finished get response crafter
    and some other stuff

--- main.py
+++ main.py
@@ -11,20 +11,20 @@ import tools
 
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-serverSocket.bind(("0.0.0.0", int()))
+serverSocket.bind(("0.0.0.0", int(tools.configuration.data["port"])))
 serverSocket.listen(tools.configuration.data["max_concurrent_connections"])
 
 while True:
     clientSocket, clientAddress = serverSocket.accept()
     request = clientSocket.recv(int(tools.configuration.data["max_request_length"]))
-    method, fileName, version = get_file_data(request.decode())
+    method, filename, version = tools.request_parsers.parse_basic_request_information(request.decode())
 
     if method == "POST":
-        content = post_response_crafter(fileName, version, request)
+        content = post_response_crafter(filename, version, request)
     elif method == "HEAD":
-        content = head_response_crafter(fileName, version)
+        content = head_response_crafter(filename, version)
     else:
-        content = get_response_crafter(fileName, version)
+        content = tools.response_crafters.get_response_crafter(filename, version)
 
     clientSocket.send(content.encode())
     clientSocket.close()
--- tools.py
+++ tools.py
@@ -2,6 +2,7 @@ import os
 
 
 class configuration:
+
     @staticmethod
     def remove_comments(line):
         if (line == "") or (line[0] == "#"):
@@ -21,11 +22,54 @@ class configuration:
             continue
         else:
             key_and_item = line_without_comments.split(" ", maxsplit=1)
+            if key_and_item[0] == "default_headers":
+                key_and_item[1] = key_and_item[1].replace(";", "\n")
             data[key_and_item[0]] = key_and_item[1]
 
 
-class file_parsers:
+class request_parsers:
 
+    @staticmethod
+    def parse_basic_request_information(data):
+        first_line = data.split("\n")[0]
+        http_method = first_line.split(" ")[0]
+        http_file = first_line.split(" ")[1]
+        http_version = first_line.split(" ")[2]
+        if http_file == "/":
+            http_file = configuration.data["default_file"]
+        else:
+            http_file = http_file[1:]
+        return http_method, http_file, http_version
+
+
+class file_parsers:
+    @staticmethod
+    def return_file_contents(file_path):
+        if os.path.isfile(configuration.data["website_directory"] + file_path):
+            file_exists = True
+            file_contents = file_renderers.choose_renderer(configuration.data["website_directory"] + file_path)
+        else:
+            file_exists = False
+            if os.path.isfile(configuration.data["website_directory"] + configuration.data["error_404_page"]):
+                file_contents = file_renderers.choose_renderer(
+                                configuration.data["website_directory"] + configuration.data["error_404_page"])
+            else:
+                file_contents = "Please configure the 404 page"
+        return file_contents, file_exists
+
+
+class response_crafters:
+    @staticmethod
+    def get_response_crafter(http_version, http_file, headers=configuration.data["default_headers"],
+                             success_status_code=configuration.data["success_status_code"]):
+        file_contents, file_exists = file_parsers.return_file_contents(http_file)
+        if file_exists:
+            return http_version + " " + success_status_code + "\n" + headers + "\n" + file_contents
+        else:
+            return http_version + " " + "404" + "\n" + headers + "\n" + file_contents
+
+
+class file_renderers:
     @staticmethod
     def default(path_to_file):
         return open(path_to_file, "r").read()[1]
@@ -35,3 +79,7 @@ class file_parsers:
         args_encapsulated = '"' + args + '"'
         args_encapsulated.replace("\r", " ")
         return os.popen(f"{command} {path_to_file} {args_encapsulated}").read()
+
+    @staticmethod
+    def choose_renderer(file_to_render):
+        return file_renderers.default(file_to_render)
